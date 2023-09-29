a7a77a0b3ee5917292a26c082a86a14b26cfcf0f
Daniel Marko <A00315052@student.tus.ie>
2023-09-29 18:49:23 +0100

    Assignment - F1 Data foundation GUI

--- Assignment/GUI_main.py
+++ Assignment/GUI_main.py
@@ -7,6 +7,9 @@ import tkinter.messagebox
 import tkinter.simpledialog
 from tkinter import *
 
+import mysql.connector
+import pandas as pd
+
 from PIL import ImageTk, Image
 
 
@@ -14,7 +17,8 @@ from PIL import ImageTk, Image
 # from M29_Frame_Obj import M29Frame
 # from WhirlPool_Frame_Obj import WhirlPoolFrame
 
-class AppGUI(tk.Frame):
+class AppGUI(tk.Frame, ):
+
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.master = master
@@ -31,40 +35,49 @@ class AppGUI(tk.Frame):
 
         self.font_1 = font.Font(family="Calibri", size=16, weight="normal")
         self.font_2 = font.Font(family="Calibri", size=12, weight="normal")
-
         # Textvariables for the values specified by the user
+
 
         self.l1 = tk.Label(master, text="Daniel Marko - Data Analytics", font=self.font_1).grid(row=0,column=1,columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W)
 
 
-
-        self.img_airbnb = ImageTk.PhotoImage(Image.open("images/airbnb.png"))
+        self.img_airbnb = ImageTk.PhotoImage(Image.open("images/anayltics.jpg").resize((400,205)))
         self.label = Label(master, image = self.img_airbnb)
         self.label.grid(row=1,column=1,columnspan=2, ipadx=10, ipady=10, padx=10, pady=10)
 
-        self.childDun_Button = tk.Button(master, text="Dun Laoghaire-Rathdown", command=self.showChildA, font=self.font_2)
-        self.childDun_Button.grid(row=2, column=1, columnspan=2,ipadx=5, ipady=5, padx=10, pady=5, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.label_search = tk.Label(master, text="Search for F1 driver name : ").grid(row=2, column=1, columnspan=2,ipadx=10, ipady=0, padx=100, pady=0,sticky=tk.N + tk.S + tk.E + tk.W)
 
-        self.childCity_Button = tk.Button(master, text="Dublin City", command=self.showChildM29, font=self.font_2)
-        self.childCity_Button.grid(row=3, column=1, columnspan=2, ipadx=5, ipady=5, padx=10, pady=5, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.ss = tk.Listbox(master)
 
-        self.childSouth_Button = tk.Button(master, text="South Dublin", command=self.showChildWhirl, font=self.font_2)
-        self.childSouth_Button.grid(row=4, column=1, columnspan=2, ipadx=5, ipady=5, padx=10, pady=5, sticky=tk.N + tk.S + tk.E + tk.W)
-
-        self.childFingal_Button = tk.Button(master, text="Fingal", command=self.showChildWhirl, font=self.font_2)
-        self.childFingal_Button.grid(row=5, column=1, columnspan=2, ipadx=5, ipady=5, padx=10, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)
+        self.driver_name = StringVar()
+        self.entry_name = tk.Entry(master, textvariable=self.driver_name).grid(row=3, column=1, columnspan=2,ipadx=10, ipady=5, padx=100, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)
+        self.search_btn = tk.Button(master, text="Search", command=self.search_query).grid(row=4, column=1, columnspan=2,ipadx=10, ipady=5, padx=100, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)
 
 
-        self.close_button = tk.Button(master, text="Close", command=self.CloseApplication, font=self.font_2, bg='red').grid(row=6,column=1,columnspan=2,ipadx=5, ipady=5, padx=10, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)
+        self.close_button = tk.Button(master, text="Close", command=self.CloseApplication, font=self.font_2, ).grid(row=6,column=1,columnspan=2,ipadx=5, ipady=5, padx=10, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)
 
-    def _layoutCanvas(self, parent):
-        img_Path = 'images/airbnb.png'
-        self.image = ImageTk.PhotoImage(file=img_Path)
-        width = self.image.width()
-        height = self.image.height()
-        self.canvas = tk.Canvas(parent, height=height, width=width)
-        self.canvas.grid(row=3, column=0, sticky=N + S + E + W)
-        self.canvas.create_image(0, 1, anchor='nw', image=self.image)
+    def search_query(self):
+        print(self.driver_name.get())
+        if self.driver_name.get() == '':
+            tkinter.messagebox.showwarning("No entry entered", "No Entry Entered, please enter a name")
+        else:
+            # Variables to connect to F1 DB
+            F1_server = 'relational.fit.cvut.cz'
+            F1_user = 'guest'
+            F1_password = 'relational'
+            F1_database = 'ErgastF1'
+            # Connection to DB
+            connection = mysql.connector.connect(
+                host=F1_server,
+                database=F1_database,
+                user=F1_user,
+                password=F1_password)
+            # Query the names for analysis
+            query_drivers_names = "SELECT concat(forename,' ', surname) as 'Drivers Names' FROM drivers where forename like '%{0}%'".format(self.driver_name.get())
+            df_mysql = pd.read_sql(query_drivers_names, con=connection)
+            values = df_mysql['Drivers Names']
+
+            print(df_mysql)
 
     def showChildA(self):
         self.ChildAB_Obj.show()
--- Assignment/assignment_driver.py
+++ Assignment/assignment_driver.py
@@ -2,17 +2,24 @@ import pandas as pd
 from tkinter import *
 from GUI_main import AppGUI
 
+
+
+F1_server = 'relational.fit.cvut.cz'
+F1_port = '3306'
+F1_user = 'guest'
+F1_password = 'relational'
+F1_database = 'ErgastF1'
+
+
 def main():
     root = Tk()
 
     myGUI = AppGUI(root)
     root.title = "Daniel Marko"
+    p1 = PhotoImage(file='images/airbnb.png')
+    root.iconphoto(False, p1)
     root.resizable(False, False)
     root.mainloop()
-
-    # df = pd.read_csv('resources/listings.csv')
-    # print(df.info())
-    # print(df['neighbourhood_cleansed'].unique())
 
 
 if __name__ == '__main__':
