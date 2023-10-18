b103c73a149b0cae3651840c064ba60a6b2016d7
Daniel Marko <A00315052@student.tus.ie>
2023-10-18 13:18:32 +0100

    Assignment - Working on fastest lap time, attempting to convert timedelta to seconds

--- Assignment/Driver_Races.py
+++ Assignment/Driver_Races.py
@@ -9,6 +9,8 @@ import webbrowser
 import pandas as pd
 import requests
 
+import matplotlib.pyplot as plt
+
 connection = mysql.connector.connect(
     host="relational.fit.cvut.cz",
     database="ErgastF1",
@@ -30,21 +32,36 @@ def get_driver_id (driver_name, connection):
 def get_driver_fastests_laps(connection):
     global str
     driver_id = get_driver_id('Lewis Hamilton', connection)
-    query_drivers_results = "SELECT * FROM results WHERE driverId = {0}".format(driver_id)
+    # Joining three tables results, races, circuits and select fastestLapTime, fastestLapSpeed, circuits.name, circuits.country
+    query_drivers_results = "SELECT fastestLapTime, fastestLapSpeed, circuits.name, circuits.country, year FROM results INNER JOIN races on results.raceId = races.raceId INNER JOIN circuits on races.circuitId = circuits.circuitId WHERE driverId = {0}".format(driver_id)
     df_mysql = pd.read_sql(query_drivers_results, con=connection)
-    # Drop any NA rows
-    df_mysql = df_mysql.dropna()
 
     # Sort by the top five fastests laps
     df_first_five_laps = df_mysql.sort_values(by=['fastestLapTime']).head(5)
     pd.set_option('display.max_columns', None)
 
-    print(df_first_five_laps)
-    print(df_first_five_laps[['fastestLapTime', 'fastestLapSpeed']])
+    df_first_five_laps['year'] = df_first_five_laps['year'].map(str)
 
-    str = ','.join(["'" + str(x) + "'" for x in df_first_five_laps['raceId'].tolist()])
+    circuits = df_first_five_laps['name'] + "-" + df_first_five_laps['country'] + " (" + df_first_five_laps['year'] + ")"
+    print(circuits)
+    fastestsTimes = df_first_five_laps['fastestLapTime']
+    df_first_five_laps['minutes'] = df_first_five_laps['fastestLapTime'].dt.seconds / 60
+    print(df_first_five_laps['minutes'])
 
-    print(str)
+    print(max(fastestsTimes))
+    print(min(fastestsTimes) )
+    pd.Timedelta
+
+    plt.bar(circuits, fastestsTimes, color = 'blue', width = 0.5)
+
+    # plt.ylim(,1.2)
+    plt.xlabel("Fastest Lap Time")
+    plt.ylabel("Circuit names and countries")
+    plt.title("Top 5 Circuits with the fastest lap times")
+    plt.show()
+
+
+
 
 if __name__ == '__main__':
     main()
